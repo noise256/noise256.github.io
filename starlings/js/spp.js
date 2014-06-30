5b3ede69e946430c96e88c41134b1f8c564de3f2
noise256 <douglashoyalcuthill@gmail.com>
2014-06-30 23:35:27 +0100

    SPP position parameter now stored temporarily.

--- starlings/js/spp.js
+++ starlings/js/spp.js
@@ -1,12 +1,14 @@
 function SPP(position, direction) {
 	SPP.velocity = 0.001;
 	
-	this.position = position;
 	this.direction = direction;
 	
 	var geometry = new THREE.BoxGeometry(1, 1, 1);
 	var material = new THREE.MeshPhongMaterial({ambient: 0x030303, color: 0x007799, specular: 0x000000, shininess: 30, shading: THREE.SmoothShading});
 	var mesh = new THREE.Mesh(geometry, material);
+	
+	mesh.position.x = position[0];
+	mesh.position.y = position[1];
 	
 	this.updateSPP = function() {
 		mesh.position.x += direction[0] * SPP.velocity;
