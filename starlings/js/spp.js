add6986135d74d30965d64017a79d8fe143e5412
noise256 <douglashoyalcuthill@gmail.com>
2014-06-30 23:06:37 +0100

    Fixed typo preventing SPP from being added.

--- starlings/js/spp.js
+++ starlings/js/spp.js
@@ -5,18 +5,19 @@ function SPP(position, direction) {
 	this.direction = direction;
 	
 	var geometry = new THREE.BoxGeometry(1, 1, 1);
-	var material = new THREE.MeshBasicMaterial({ambient: 0x030303, color: 0x007799, specular: 0x000000, shininess: 30, shading: THREE.SmoothShading});
+	var material = new THREE.MeshPhongMaterial({ambient: 0x030303, color: 0x007799, specular: 0x000000, shininess: 30, shading: THREE.SmoothShading});
 	var mesh = new THREE.Mesh(geometry, material);
 	
 	this.updateSPP = function() {
-		//position[0] += direction[0] * SPP.velocity;
-		//position[1] += direction[1] * SPP.velocity;
-		//mesh.rotation.x += 0.001;
+		mesh.position.x += direction[0] * SPP.velocity;
+		mesh.position.y += direction[1] * SPP.velocity;
+		
+		mesh.rotation.x += 0.01;
 	};
 	
 	this.getMesh = function() {
-		//mesh.position.x = position[0];
-		//mesh.position.y = position[1];
+		mesh.position.x = position[0];
+		mesh.position.y = position[1];
 		
 		return mesh;
 	};
