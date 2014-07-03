eefabce6d134a92b3b1d4f1eb905b4369a336df6
noise256 <douglashoyalcuthill@gmail.com>
2014-07-03 01:29:16 +0100

    SPP direction init fix.
    Change pivot of rotation calculation used to generate SPP orientation
    from [1, 0] to [0, 0].

--- starlings/js/spp.js
+++ starlings/js/spp.js
@@ -7,7 +7,7 @@ function SPP(position, direction) {
 	var direction = direction;
 	
 	var geometry = new THREE.SphereGeometry(0.075, 32, 32);
-	var material = new THREE.MeshPhongMaterial({ambient: 0x030303, color: 0x667799, specular: 0xffffff, shininess: 10, shading: THREE.SmoothShading});
+	var material = new THREE.MeshBasicMaterial({ambient: 0x030303, color: 0x667799, specular: 0xffffff, shininess: 10, shading: THREE.SmoothShading});
 	var mesh = new THREE.Mesh(geometry, material);
 	
 	this.updateSPP = function(siblings) {
