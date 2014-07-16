cda3ed47b74692b1f3ef073c1365268973b98564
noise256 <douglashoyalcuthill@gmail.com>
2014-07-16 18:07:42 +0100

    Test

--- starlings/js/SPPModel.js
+++ starlings/js/SPPModel.js
@@ -58,7 +58,7 @@ function initGUI() {
 function initRenderer() {
 	scene = new THREE.Scene();
 	
-	scene.fog = new THREE.FogExp2( 0x5fa5d8, 0.0025 );
+	//scene.fog = new THREE.FogExp2( 0x5fa5d8, 0.0025 );
 	
 	camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 1000);				
 	camera.position.x = 24;
