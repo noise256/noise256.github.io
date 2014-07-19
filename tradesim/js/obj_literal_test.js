3a3648d4e1673992bd849331d45d0eaab46645b4
noise256 <douglashoyalcuthill@gmail.com>
2014-07-19 19:39:27 +0100

    Test

---
+++ tradesim/js/obj_literal_test.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	renderer.initRenderer();
+	renderer.initModel();
+	renderer.frame();
+}
+
+var renderer {
+	canvasWidth: 1000,
+	canvasHeight: 800,
+	
+	init: function() {
+		alert(x + y);
+	}
+}
--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -2,13 +2,16 @@ var renderer = {
 	canvasWidth: 1000,
 	canvasHeight: 800,
 	
-	fpsMeter: new FPSMeter(document.body, { decimals: 0, graph: true, theme: 'dark', left: '5px' }),
+	fpsMeter: null, new FPSMeter(document.body, { decimals: 0, graph: true, theme: 'dark', left: '5px' }),
 	
-	scene: new THREE.Scene(),
-	camera: new THREE.PerspectiveCamera(60, this.canvasWidth / this.canvasHeight, 0.1, 1000),
-	renderer: new THREE.WebGLRenderer(),
+	scene: null, 
+	camera: null, 
+	renderer: null,
 	
-	initRenderer: function() {		
+	initRenderer: function() {
+		scene = new THREE.Scene();
+		
+		camera = new THREE.PerspectiveCamera(60, this.canvasWidth / this.canvasHeight, 0.1, 1000);
 		this.camera.position.x = 24;
 		this.camera.position.y = 10;
 		this.camera.position.z = 24;
@@ -18,13 +21,13 @@ var renderer = {
 		controls.target.y = 10;
 		
 		var ambientLight = new THREE.AmbientLight(0x404040);
-		
 		var mainLight = new THREE.PointLight();
 		mainLight.position.set(50, 150, 150);
 		
 		this.scene.add(ambientLight);
 		this.scene.add(mainLight);
-				
+		
+		renderer = new THREE.WebGLRenderer();
 		this.renderer.setSize(this.canvasWidth, this.canvasHeight);
 		this.renderer.setClearColor(0xffffff, 1);
 		
