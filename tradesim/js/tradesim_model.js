b9ef78eb0c49ade16b5b4d218ebf14a52c4bb360
noise256 <douglashoyalcuthill@gmail.com>
2014-07-22 04:41:08 +0100

    tradesim_model.js
    Expanded outline.

--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -1,68 +1,96 @@
 window.onload = function() {
-	TradeSimModel.init();
-	TradeSimModel.frame();
+	SimulationView.init();
+	SimulationController.init();
+	
+	SimulationView.frame();
 }
 
-var TradeSimModel = {
+var SimulationView = {
 	canvasWidth: 1000,
 	canvasHeight: 800,
 	
 	fpsMeter: null, 
 	
-	scene: null, 
+	scene: null,
 	camera: null, 
 	renderer: null,
 	
 	init: function() {
 		fpsMeter = new FPSMeter(document.body, { decimals: 0, graph: true, theme: 'dark', left: '5px' });
 		
-		scene = new THREE.Scene();
+		SimulationView.scene = new THREE.Scene();
 		
-		camera = new THREE.PerspectiveCamera(60, TradeSimModel.canvasWidth / TradeSimModel.canvasHeight, 0.1, 1000);
-		camera.position.x = 24;
-		camera.position.y = 10;
-		camera.position.z = 24;
-		camera.lookAt(new THREE.Vector3(0, 10, 0));
+		SimulationView.camera = new THREE.PerspectiveCamera(60, SimulationView.canvasWidth / SimulationView.canvasHeight, 1, 10000);
+		SimulationView.camera.position.x = 24;
+		SimulationView.camera.position.y = 10;
+		SimulationView.camera.position.z = 24;
+		SimulationView.camera.lookAt(new THREE.Vector3(0, 10, 0));
 		
-		var controls = new THREE.OrbitControls(camera, document.getElementById("canvas"));
+		var controls = new THREE.OrbitControls(SimulationView.camera, document.getElementById("canvas"));
 		controls.target.y = 10;
 		
 		var ambientLight = new THREE.AmbientLight(0x404040);
 		var mainLight = new THREE.PointLight();
 		mainLight.position.set(50, 150, 150);
 		
-		scene.add(ambientLight);
-		scene.add(mainLight);
+		SimulationView.scene.add(ambientLight);
+		SimulationView.scene.add(mainLight);
 		
-		renderer = new THREE.WebGLRenderer();
-		renderer.setSize(TradeSimModel.canvasWidth, TradeSimModel.canvasHeight);
-		renderer.setClearColor(0xffffff, 1);
+		SimulationView.renderer = new THREE.WebGLRenderer();
+		SimulationView.renderer.setSize(SimulationView.canvasWidth, SimulationView.canvasHeight);
+		SimulationView.renderer.setClearColor(0x000000, 1);
 		
-		document.getElementById("canvas").appendChild(renderer.domElement);
+		document.getElementById("canvas").appendChild(SimulationView.renderer.domElement);
 	},
 	
 	update: function() {
+		SimulationController.update();
 	},
 	
 	render: function() {
-		if (TradeSimModel.renderer) {
-			TradeSimModel.renderer.render(TradeSimModel.scene, TradeSimModel.camera);
+		if (SimulationView.renderer) {
+			SimulationView.renderer.render(SimulationView.scene, SimulationView.camera);
 		}
 	},
 	
 	frame: function() {
 		fpsMeter.tickStart();
 
-		TradeSimModel.update();
-		TradeSimModel.render();
+		SimulationView.update();
+		SimulationView.render();
 		
 		fpsMeter.tick();
 		
-		requestAnimationFrame(TradeSimModel.frame);
+		requestAnimationFrame(SimulationView.frame);
 	}
 }
 
 var SimulationController = {
+	numPlanets:50,
+	
+	planets:[],
+	colonies:[],
+	traders:[],
+	
+	init:function() {
+		//create planets
+		for (var i = 0; i < SimulationController.numPlanets; i++) {
+			var planetBody = new Body(vec3.random(vec3.create(), Math.random() * 1000), 0);
+			var planetView = new View(new THREE.SphereGeometry(5, 32, 32), new THREE.MeshPhongMaterial({color: 0x443300}));
+			SimulationController.planets.push(new Planet(planetBody, planetView));
+			
+			if (SimulationView.scene) {
+				SimulationView.scene.add(planetView.mesh);
+			}
+		}
+	},
+	
+	update:function() {
+		//update colonies and traders using TraderController and ColonyController
+		for (var i = 0; i < SimulationController.planets.length; i++) {
+			SimulationController.planets[i].update();
+		}
+	}
 }
 
 var TraderController = {
@@ -71,29 +99,48 @@ var TraderController = {
 var ColonyController = {
 }
 
-var ShipController = {
-}
-
 //TODO is an object needed for resource? Should it just be a value on Planet, Ship and Colony objects? How to do enum that defines resource chain? Should each container have a single Resources object that contains the name and quantity of each resource?
 var Resources = function(params) {
 }
 
-var Body = function(params) {
-	var position;
-	var rotation;
+function Body(position, velocity) {
+	this.position = position;
+	this.velocity = velocity;
 }
 
-var Planet = function(params) {
-	var body;
+Body.prototype = {
+	move:function(direction) {
+		vec3.scaleAndAdd(this.position, this.position, direction, this.velocity); 
+	}
 }
 
-var StarSystem = function(params) {
-	var body;
+function View(geometry, material, position) {
+	var geometry = geometry;
+	var material = material;
+	
+	this.mesh = new THREE.Mesh(geometry, material);
 }
 
-var Trader = function(params) {
-	var body;
+View.prototype = {
+	update:function(position) {
+		this.mesh.position.set(position[0], position[1], position[2]);
+	}
 }
 
-var Colony = function(params) {
+function Planet(body, view) {
+	this.body = body;
+	this.view = view;
+	this.colonies = [];
+}
+
+Planet.prototype = {
+	update:function() {
+		this.view.update(this.body.position);
+	}
+}
+
+function Trader() {
+}
+
+function Colony() {
 }
