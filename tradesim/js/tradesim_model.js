cb3faa1216bcb51a7fd967af1bc5ee69f347afb3
noise256 <douglashoyalcuthill@gmail.com>
2014-07-27 13:56:54 +0100

    Debug message for object intersection reports object string instead of id.

--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -65,8 +65,8 @@ var SimulationView = {
 		var raycaster = SimulationView.projector.pickingRay(SimulationView.mouseVector.clone(), SimulationView.camera),
 		intersects = raycaster.intersectObjects( SimulationView.scene.children );
 		
-		for (var i = 0;i < intersects.length; i++) {
-			console.warn('intersects ' + intersects[i].id);
+		for (var i = 0; i < intersects.length; i++) {
+			console.warn('intersects ' + intersects[i]);
 		}
 	},
 	
