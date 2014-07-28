2dcacb791434e6931d0eaf6a95143bfe1c4d81fe
noise256 <douglashoyalcuthill@gmail.com>
2014-07-28 12:34:40 +0100

    Simplified trader geometry and material.

--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -266,8 +266,8 @@ var SimulationController = {
 		}
 		
 		//create traders
-		var traderGeometry = new THREE.SphereGeometry(1, 32, 32);
-		var traderMaterial = new THREE.MeshPhongMaterial({color: 0x003344});
+		var traderGeometry = new THREE.BoxGeometry(1, 1, 1);
+		var traderMaterial = new THREE.MeshBasicMaterial({color: 0x003344});
 		for (var i = 0; i < SimulationController.numTraders; i++) {
 			var traderPosition = vec3.random(vec3.create(), Math.random() * SimulationController.maxPlanetSpread); //TODO implement max trader spread
 			var traderBody = new Body(traderPosition, 1, 0.005, 0.3);
