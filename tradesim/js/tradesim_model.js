c01d34201a2b5548b71295522a1c5bbc6c4cb361
noise256 <douglashoyalcuthill@gmail.com>
2014-07-29 01:52:59 +0100

    Debugging.

--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -261,8 +261,8 @@ var SimulationController = {
 		//create traders
 		var traderGeometry = new THREE.BoxGeometry(1, 1, 1);
 		var traderMaterial = new THREE.ShaderMaterial({
-			vertexShader: $('vertexshader').text(),
-			fragmentShader: $('fragmentshader').text(),
+			vertexShader: $('#vertexshader').text(),
+			fragmentShader: $('#fragmentshader').text(),
 		});
 		
 		//var traderMaterial = new THREE.MeshBasicMaterial({color: 0x003344});
