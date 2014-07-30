27214fad1286ea08b734eebadf0b33734becdf66
noise256 <douglashoyalcuthill@gmail.com>
2014-07-30 02:43:11 +0100

    Debugging.

--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -247,7 +247,7 @@ var SimulationController = {
 			planetMaterial.uniforms = {
 				planetTexture: {type: "t", value: THREE.ImageUtils.loadTexture('images/sedna.jpg')},
 				cameraHeight2: {type:'f', value: 0},
-				lightDir: {type:'v3', value: new THREE.Vector3(1.0, 0.0, 0.0)},//{type:'v3', value: new THREE.Vector3(0, 0, 0).subVectors(planetPosition, SimulationView.planetLight.position).normalize()},
+				lightDir: {type:'v3', value: new THREE.Vector3(1e8, 0, 1e8).normalize()},//{type:'v3', value: new THREE.Vector3(0, 0, 0).subVectors(planetPosition, SimulationView.planetLight.position).normalize()},
 				invWaveLength: {type:'v3', value: new THREE.Vector3(1.0/Math.pow(0.650,4), 1.0/Math.pow(0.570,4), 1.0/Math.pow(0.475,4))},
 				outerRadius: {type:'f', value:5.125},
 				outerRadius2: {type:'f', value:26.265625},
