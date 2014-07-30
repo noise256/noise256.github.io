567c92130dfd2fe3f895fb0cb11c62ee397c9ee9
noise256 <douglashoyalcuthill@gmail.com>
2014-07-30 05:48:28 +0100

    Debugging.

--- tradesim/js/tradesim_model.js
+++ tradesim/js/tradesim_model.js
@@ -349,10 +349,8 @@ var SimulationController = {
 	update:function() {
 		//update colonies and traders using TraderController and ColonyController
 		for (var i = 0; i < SimulationController.planets.length; i++) {
-			var temp = new THREE.Vector3();
-			var camPos = SimulationView.camera.position;
 			var planetPos = SimulationController.planets[i].body.position;
-			var cameraHeight = temp.subVectors(camPos, planetPos).length();
+			var cameraHeight = new THREE.Vector3().subVectors(SimulationView.camera.position, new THREE.Vector3(planetPos[0], planetPos[1], planetPos[2]).length();
 			for (var j = 0; j < SimulationController.planets[i].view.meshes.length; j++) {
 				SimulationController.planets[i].view.meshes[j].material.uniforms.cameraHeight2.value = cameraHeight * cameraHeight;
 			}
