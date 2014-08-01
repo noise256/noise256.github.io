fe64523e9efb7b09dc9de3d4c1324989303cb383
noise256 <douglashoyalcuthill@gmail.com>
2014-08-01 02:50:02 +0100

    Lowered zoom speed of camera.

--- tradesim/js/lib/TrackballControls.js
+++ tradesim/js/lib/TrackballControls.js
@@ -18,7 +18,7 @@ THREE.TrackballControls = function ( object, domElement ) {
 	this.screen = { left: 0, top: 0, width: 0, height: 0 };
 
 	this.rotateSpeed = 1.0;
-	this.zoomSpeed = 1.2;
+	this.zoomSpeed = 0.5;
 	this.panSpeed = 0.3;
 
 	this.noRotate = false;
