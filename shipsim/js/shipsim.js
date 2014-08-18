8c01b4c321329b538b40f900fda298874c2eebe5
noise256 <douglashoyalcuthill@gmail.com>
2014-08-18 16:28:59 +0100

    Added prototype methods to simulation view object.

--- shipsim/js/shipsim.js
+++ shipsim/js/shipsim.js
@@ -41,3 +41,49 @@ function SimulationView() {
 		this.fpsMeter.tick();
 	}
 }
+
+SimulationView.prototype = {
+	init: function() {
+		this.scene = new THREE.Scene();
+		
+		this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 10, 10000000);
+		this.camera.position.x = 0;
+		this.camera.position.y = 3000;
+		this.camera.position.z = 25000;
+		this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+		
+		this.renderer = new THREE.WebGLRenderer({antialias: true});
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.setClearColor(0x000000, 1);
+		
+		this.controls = new THREE.OrbitControls(this.camera);
+		this.controls.maxDistance = 10000000;
+		this.controls.xRotateSpeed = 1.0;
+		this.controls.yRotateSpeed = 0.025;
+		this.controls.panSpeed = 0.05;
+		
+		this.fpsMeter = new FPSMeter(document.body, { decimals: 0, graph: true, theme: 'dark', left: '5px' });
+		
+		document.getElementById("canvas").appendChild(this.renderer.domElement);
+			
+		THREEx.WindowResize(this.renderer, this.camera);
+		THREEx.FullScreen.bindKey({charCode: 'f'.charCodeAt(0)});
+	},
+	
+	render: function() {
+		this.renderer.render(this.scene, this.camera);
+	},
+	
+	updateCamera: function() {
+		if (this.cameraDestination && this.cameraProgress <= 1.0) {
+			var cameraStep = this.orgCameraPosition.clone().lerp(this.cameraDestination, this.cameraProgress);
+			
+			this.camera.position.set(cameraStep.x, cameraStep.y, cameraStep.z);
+			this.controls.savePosition();
+			this.controls.saveTarget();
+			this.controls.reset();
+			
+			this.cameraProgress += 0.1;
+		}
+	}
+}
